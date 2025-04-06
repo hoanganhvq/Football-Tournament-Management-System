10beda73d168fb6fda47397f473548ecae04891c
Anh Phan <anhphan42891@gmail.com>
2025-04-06 18:13:51 +0700

    change secure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,7 @@ const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "lax", // Adjusted for cross-origin
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
